d5fb9d9d0ee48f78daf537c9ad3ceadfc13c2b1f
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-03-24 14:10:33 -0400

    Update App.js

--- favgamelist/src/App.js
+++ favgamelist/src/App.js
@@ -39,6 +39,7 @@ function App() {
       headers:{
         'Content-Type':'application/json'
       },
+      //Do not pass extra object into the request (it's removing only the name of student when I deactivate)
       body: JSON.stringify({...student, active:false})
     })
     .then(res => res.json())
